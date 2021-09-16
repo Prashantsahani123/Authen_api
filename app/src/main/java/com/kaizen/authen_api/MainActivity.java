8c3a5ab5b5dbffdd9bdc43140f5af91878187398
prashant.sahani@kaizeninfotech.com <prashantsahani123456789>
2021-09-16 14:30:45 +0530

    authenticate api

--- app/src/main/java/com/kaizen/authen_api/MainActivity.java
+++ app/src/main/java/com/kaizen/authen_api/MainActivity.java
@@ -20,18 +20,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //String strRequestData = Utils.replaceNewLine(AES_BC.getInstance().encryptEncode(jsonRequestDataObj.toString(),  Constants.CLIENT_REQUEST_ENCRYPTION_KEY));
 
-        apiInterface = APIClient.getClient().create(APIInterface.class);
+         apiInterface = APIClient.getClient().create(APIInterface.class);
         Call<JsonObject> call = apiInterface.getallcustomer();
 //        Call<JsonObject> call = apiInterface.gettoken("7666660143","Pass@123","password");
         call.enqueue(new Callback<JsonObject>() {
             @Override
             public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
 
-
                 Log.d("TAG",response.code()+"");
 
                 String displayResponse = String.valueOf(response.body());
+
+
                 Log.d("tokenn",displayResponse);
 
 
